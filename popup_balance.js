f5cc46e0f0da47c6d6f17fc24070848ec2dde879
PepritSahrs <83342294+PepritSahrs@users.noreply.github.com>
2024-01-04 18:07:59 +0100

    Popup Balance
    Das PopUp für das Saldo wurde samt Skript hinzugefügt. Ein paar Anpassung an der Sidecar wurden ebenfalls gemacht

---
+++ popup_balance.js
@@ -0,0 +1,7 @@
+function openPopup() {
+	document.getElementById('sidebar_overlay_bal').style.display = 'flex';
+	}
+
+function closePopup() {
+    document.getElementById('sidebar_overlay_bal').style.display = 'none';
+    }
