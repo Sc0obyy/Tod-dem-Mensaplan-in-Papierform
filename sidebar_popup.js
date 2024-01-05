a49b316543985a19eab95fcd9bbc2f4641c37731
PepritSahrs <83342294+PepritSahrs@users.noreply.github.com>
2024-01-05 12:08:46 +0100

    Final version of sidebar and scan screen with popups

---
+++ sidebar_popup.js
@@ -0,0 +1,12 @@
+function open_bal() {
+	document.getElementById('sidebar_overlay_bal').style.display = 'flex';
+	}
+	
+function open_pref() {
+	document.getElementById('sidebar_overlay_pref').style.display = 'flex';
+	}
+
+function closePopup() {
+    document.getElementById('sidebar_overlay_bal').style.display = 'none';
+    document.getElementById('sidebar_overlay_pref').style.display = 'none';
+    }
