c6e41da80e8cbf230611dcec859672884c99e3ac
dean.hoffmann@schulprojekte.uni-hannover.de <dean.hoffmann@stud.uni-hannover.de>
2024-01-06 21:25:06 +0100

    structural changes

--- day-view.js
+++ day-view.js
@@ -1,16 +1,17 @@
-var filterArray = ["Meine Vorlieben", "Keine Nüsse", "Vegan", "Vegetarisch", "Glutenfrei", "Kaktosefrei", "High Protein"];
+var filterArray = [];
+// "Meine Vorlieben", "Keine Nüsse", "Vegan", "Vegetarisch", "Glutenfrei", "Laktosefrei", "High Protein", "Mit Fleisch"
 
 function displayDropdown(id) {
   document.getElementById(id).classList.toggle("show");
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (!event.target.matches('.dropdown-button')) {
     var dropdownArray = []
     dropdownArray.push(document.getElementById("day-selection-dropdown"));
     dropdownArray.push(document.getElementById("filter-selection-dropdown"));
     for (let i = 0; i < dropdownArray.length; i++) {
-      if(dropdownArray[i].classList.contains('show')) {
+      if (dropdownArray[i].classList.contains('show')) {
         dropdownArray[i].classList.remove('show');
       }
     }
@@ -22,23 +23,68 @@ function displayDay(day) {
 }
 
 function addFilter(filter) {
-  if (filterArray.includes(filter)) {
+  if (!filterArray.includes(filter)) {
+    filterArray.push(filter);
     const node = document.createElement("li");
     const textNode = document.createTextNode(filter);
     node.classList.add("selected-filter");
     node.appendChild(textNode);
     document.getElementById("filter-list").appendChild(node);
+    
+  } else {
     const itemIndex = filterArray.indexOf(filter);
     if (itemIndex > -1) {
       filterArray.splice(itemIndex, 1);
     }
-  } else {
-    filterArray.push(filter);
     var myList = document.getElementById("filter-list");
-    myList.querySelectorAll('li').forEach(function(item) {
+    myList.querySelectorAll('li').forEach(function (item) {
       if (item.innerHTML === filter) {
         item.remove();
       }
     })
   }
 }
+
+
+
+function addIconList(arr) {
+  let string = ""
+  for (let i = 0; i < arr.length; i++) {
+    string = string.concat("", arr[i], "", "\n")
+  }
+  return string
+}
+
+
+function createCard(dish) {
+  const cardContainer = document.getElementById("dishes-list");
+
+  cardContainer.innerHTML += `
+  <li class="dish-card">
+    <div class="card-image">
+      ${dish.imageURL}
+    </div>
+    <div class="card-description">
+      <p class="card-dish-name">${dish.name}</p>
+      <div class="card-dish-merkmale">
+        ${addIconList(dish.FilterIcons)}
+      </div>
+      <div class="card-information">
+        <div class="card-dish-bewertung">
+          <img class="rating-icon" alt="thumbs up icon" src="resources\\icons\\thumbs-up.png">
+          <p>${dish.rating}</p>
+        </div>
+        <p class="card-dish-preis">${dish.price}</p>
+      </div>
+    </div>
+  </li>
+  `
+}
+
+for (let i = 0; i < mondayDishes.length; i++) {
+  createCard(mondayDishes[i]);
+}
+
+
+
+
---
+++ monday_dishes.js
@@ -0,0 +1,70 @@
+
+const mondayDishes = [
+  {
+    imageURL: "<img class=\"food-image\" alt=\"curry\" src=\"resources\\curry.jpeg\">",
+    name: "Tofu Curry mit Reis",
+    Filter: ["Vegan", "Keine Nüsse", "High Protein", "Glutenfrei", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegan.png\">",
+      "<img class=\"icon\" alt=\"protein icon\" src=\"resources\\icons\\muscles.png\">"
+    ],
+    rating: "69%",
+    price: "3,00€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"nudeln\" src=\"resources\\nudeln.jpeg\">",
+    name: "Nulden mit Tomatensauce",
+    Filter: ["Vegan", "Keine Nüsse", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegan.png\">",
+      "<img class=\"icon\" alt=\"gluten icon\" src=\"resources\\icons\\gluten.png\">"
+    ],
+    rating: "55%",
+    price: "1,60€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"currywurst\" src=\"resources\\currywurst.jpeg\">",
+    name: "Currywurst Pommes",
+    Filter: ["High Protein", "Keine Nüsse", "Mit Fleisch", "Glutenfrei", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"protein icon\" src=\"resources\\icons\\muscles.png\">",
+      "<img class=\"icon\" alt=\"pork icon\" src=\"resources\\icons\\pig.png\">"
+    ],
+    rating: "90%",
+    price: "3,00€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"pudding\" src=\"resources\\pudding.jpeg\">",
+    name: "Schokopudding",
+    Filter: ["Vegetarisch", "Glutenfrei", "Keine Nüsse"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegetarian icon\" src=\"resources\\icons\\vegetarian.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">"
+    ],
+    rating: "70%",
+    price: "0,80€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"schnitzel\" src=\"resources\\schnitzel.jpeg\">",
+    name: "Schnitzel",
+    Filter: ["Glutenfrei", "High Protein", "Mit Fleisch", "Keine Nüsse", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"protein icon\" src=\"resources\\icons\\muscles.png\">",
+      "<img class=\"icon\" alt=\"pork icon\" src=\"resources\\icons\\pig.png\">"
+    ],
+    rating: "87%",
+    price: "4,00€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"salat\" src=\"resources\\salat.jpeg\">",
+    name:"Italienischer Salat",
+    Filter: ["Vegetarisch", "Glutenfrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegetarian icon\" src=\"resources\\icons\\vegetarian.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">",
+      "<img class=\"icon\" alt=\"nut icon\" src=\"resources\\icons\\nut.png\">"
+    ],
+    rating: "74%",
+    price: "1,50€"
+  }
+]
