684515cbeb7cb3f318264192b766ee4bb19d224f
Skrolli3e <elijahjereduy@gmail.com>
2024-01-08 15:59:53 +0100

    added links between food

--- Week_view.js
+++ Week_view.js
@@ -1,23 +1,35 @@
-function addDish(dish, id) {
+
+function addDish(dish, id, index, currentDay) {
     const dishContainer = document.getElementById(id)
     dishContainer.innerHTML += `
-    <li class="dish-card">
-                        <div class = "dish-card-image">
-                            ${dish.imageURL}
-                        </div>
-                        <div class="dish-name">${dish.name}</div>
-                        <div class="dish-information">
-                            <div class="dish-rating">${dish.rating}</div>
-                            <div class="dish-price">${dish.price}</div>
-                        </div>
-                    </li>
+    <li class="dish-card">     
+        <button class="dish-card-image" onclick="seeInfo('${currentDay}', '${index}')">
+            ${dish.imageURL}
+        </button>
+        <div class="dish-name">${dish.name}</div>
+        <div class="dish-information">
+            <div class="dish-rating">${dish.rating}</div>
+            <div class="dish-price">${dish.price}</div>
+        </div>
+    </li>
     `
 }
 
+var currentDay = 0
+
+function seeInfo(day, index) {
+    localStorage.setItem("day", day)
+    localStorage.setItem("index", index)
+    window.location.href = "detail_view.html"
+}
+
 function renderDishes(dayDishes,id) {
+    index = 0
     dayDishes.forEach(e =>{
-        addDish(e, id);
+        addDish(e, id, index, currentDay);
+        index += 1
     })
+    currentDay += 1
 }
 
 renderDishes(mondayDishes, "monday-dishes")
--- detail_view.js
+++ detail_view.js
@@ -1,3 +1,51 @@
+
 function goBack() {
     window.history.back();
 }
+function convertIntToDay(day){
+    if (day == 0 || day == 2 || day == 4 ){
+        return mondayDishes
+    } else
+    return tuesdayDishes
+}
+function addFoodInfo(day, index) {
+    dish = day[index]
+    const foodContainer = document.getElementById("food-info")
+    foodContainer.innerHTML += `
+    <div id="app">
+        <div id="closeButton" onclick="goBack()">
+            <span>&#9665;</span>
+        </div>
+        <h1 class="dish-name">${dish.name}</h1>
+        <div id="imageContainer">
+            ${dish.imageURL}
+        </div>
+        <div id="nutritionalInfo">
+            <h2>Nährwerte:</h2>
+            <div class="nutrition">
+                <p>Kalorien: 550 kcal</p>
+                <p>Protein: 12 g</p>
+                <p>Kohlenhydrate: 65 g</p>
+                <p>Fett: 30 g</p>
+            </div>
+        </div>
+        <div id="notesAndAllergens">
+            <h2>Hinweise:</h2>
+                <ul id="Hinweise">
+                </ul>   
+        </div>
+    </div>`
+}
+function addAllergies(day, index){
+    const allergyContainer = document.getElementById("Hinweise")
+    day[index].Filter.forEach(element => {
+        if (element != "Meine Vorlieben"){
+            allergyContainer.innerHTML +=`
+            <p>${element}</p>
+            `
+        }
+    });
+}
+addFoodInfo(convertIntToDay(localStorage.getItem("day")), localStorage.getItem("index"))
+addAllergies(convertIntToDay(localStorage.getItem("day")), localStorage.getItem("index"))
+
