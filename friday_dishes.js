cabb8cda10928792e2755add9be0de17027539a0
PepritSahrs <83342294+PepritSahrs@users.noreply.github.com>
2024-01-19 17:11:08 +0100

    Add files via upload

---
+++ friday_dishes.js
@@ -0,0 +1,72 @@
+
+
+
+const mondayDishes = [
+  {
+    imageURL: "<img class=\"food-image\" alt=\"curry\" src=\"resources\\monday_images\\curry.jpeg\">",
+    name: "Tofu Curry mit Reis",
+    Filter: ["Meine Vorlieben", "Vegan", "Keine Nüsse", "High Protein", "Glutenfrei", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegan.png\">",
+      "<img class=\"icon\" alt=\"protein icon\" src=\"resources\\icons\\muscles.png\">"
+    ],
+    rating: "69%",
+    price: "3,00€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"nudeln\" src=\"resources\\monday_images\\nudeln.jpeg\">",
+    name: "Nulden mit Tomatensauce",
+    Filter: ["Meine Vorlieben", "Vegan", "Keine Nüsse", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegan.png\">",
+      "<img class=\"icon\" alt=\"gluten icon\" src=\"resources\\icons\\gluten.png\">"
+    ],
+    rating: "55%",
+    price: "1,60€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"currywurst\" src=\"resources\\monday_images\\currywurst.jpeg\">",
+    name: "Currywurst Pommes",
+    Filter: ["Meine Vorlieben", "High Protein", "Keine Nüsse", "Mit Fleisch", "Glutenfrei", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"protein icon\" src=\"resources\\icons\\muscles.png\">",
+      "<img class=\"icon\" alt=\"pork icon\" src=\"resources\\icons\\pig.png\">"
+    ],
+    rating: "90%",
+    price: "3,00€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"pudding\" src=\"resources\\monday_images\\pudding.jpeg\">",
+    name: "Schokopudding",
+    Filter: ["Meine Vorlieben", "Vegetarisch", "Glutenfrei", "Keine Nüsse"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegetarian icon\" src=\"resources\\icons\\vegetarian.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">"
+    ],
+    rating: "70%",
+    price: "0,80€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"schnitzel\" src=\"resources\\monday_images\\schnitzel.jpeg\">",
+    name: "Schnitzel",
+    Filter: ["Meine Vorlieben", "Glutenfrei", "High Protein", "Mit Fleisch", "Keine Nüsse", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"protein icon\" src=\"resources\\icons\\muscles.png\">",
+      "<img class=\"icon\" alt=\"pork icon\" src=\"resources\\icons\\pig.png\">"
+    ],
+    rating: "87%",
+    price: "4,00€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"salat\" src=\"resources\\monday_images\\salat.jpeg\">",
+    name:"Italienischer Salat",
+    Filter: ["Meine Vorlieben", "Vegetarisch", "Glutenfrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegetarian icon\" src=\"resources\\icons\\vegetarian.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">",
+      "<img class=\"icon\" alt=\"nut icon\" src=\"resources\\icons\\nut.png\">"
+    ],
+    rating: "74%",
+    price: "1,50€"
+  }
+]
---
+++ thursday_dishes.js
@@ -0,0 +1,75 @@
+
+
+
+const thursdayDishes = [
+  {
+   {
+    imageURL: "<img class=\"food-image\" alt=\"nudeln\" src=\"resources\\thursday_images\\nudeln.jpeg\">",
+    name: "Nulden mit Tomatensauce",
+    Filter: ["Meine Vorlieben", "Vegan", "Keine Nüsse", "Laktosefrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegan.png\">",
+      "<img class=\"icon\" alt=\"gluten icon\" src=\"resources\\icons\\gluten.png\">"
+    ],
+    rating: "55%",
+    price: "1,60€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"curry\" src=\"resources\\thursday_images\\linsengulasch.jpeg\">",
+    name: "Linsen-Gulasch, Brokkoli, Kartoffel-Möhrenpüree",
+    Filter: ["Meine Vorlieben", "Vegan","Mit Soja" ],
+    FilterIcons: [
+     "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegan.png\">"
+    ],
+    rating: "68%",
+    price: "3,20€"
+  },
+   {
+    imageURL: "<img class=\"food-image\" alt=\"Pizza\" src=\"resources\\thursday_images\\pizza_brokkoli.jpeg\">",
+    name: "Pizza mit Brokkoli und Hollandaise",
+    Filter: ["Vegetarisch", "Keine Nüsse"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegan.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">",
+      "<img class=\"icon\" alt=\"gluten icon\" src=\"resources\\icons\\gluten.png\">"
+    ],
+    rating: "79%",
+    price: "4,95€"
+  },
+  {
+    imageURL: "<img class=\"food-image\" alt=\"weiße Schokolade Pudding\" src=\"resources\\thursday_images\\weiße_schokolade_pudding.jpeg\">",
+    name: "Weiße Schokolade Pudding mit Orangen",
+    Filter: ["Vegetarisch", "Keine Nüsse", "Glutenfrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegan icon\" src=\"resources\\icons\\vegetarian.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">"
+    ],
+    rating: "36%",
+    price: "0,80€"
+  },
+   {
+    imageURL: "<img class=\"food-image\" alt=\"salat\" src=\"resources\\thursday_images\\salat.jpeg\">",
+    name:"Italienischer Salat",
+    Filter: ["Meine Vorlieben", "Vegetarisch", "Glutenfrei"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"vegetarian icon\" src=\"resources\\icons\\vegetarian.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">",
+      "<img class=\"icon\" alt=\"nut icon\" src=\"resources\\icons\\nut.png\">"
+    ],
+    rating: "74%",
+    price: "1,50€"
+  },
+   {
+    imageURL: "<img class=\"food-image\" alt=\"salat\" src=\"resources\\thursday_images\\backschinken.jpeg\">",
+    name:"Backschinken vom Schwein mit Bratensauce und Kartoffeln",
+    Filter: ["Mit Fleisch", "High Protein", "Glutenfrei", "Keine Nüsse"],
+    FilterIcons: [
+      "<img class=\"icon\" alt=\"protein icon\" src=\"resources\\icons\\muscles.png\">",
+      "<img class=\"icon\" alt=\"pork icon\" src=\"resources\\icons\\pig.png\">",
+      "<img class=\"icon\" alt=\"dairy icon\" src=\"resources\\icons\\dairy-products.png\">"
+    ],
+    rating: "74%",
+    price: "3,20€"
+  }
+]
+  
